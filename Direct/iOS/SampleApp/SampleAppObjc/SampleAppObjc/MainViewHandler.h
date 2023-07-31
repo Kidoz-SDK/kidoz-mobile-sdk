b643de5b9d22d378082cb5fbb92d2ff105476c6f
Maria_Yelfimova <maria@kidoz.net>
2023-07-31 15:10:59 +0300

    add MainViewHandler public function to the .h

--- Direct/iOS/SampleApp/SampleAppObjc/SampleAppObjc/MainViewHandler.h
+++ Direct/iOS/SampleApp/SampleAppObjc/SampleAppObjc/MainViewHandler.h
@@ -9,6 +9,45 @@
 #import <CommonSampleApp/CommonSampleApp.h>
 #import <KidozSDK/KidozSDK.h>
 
-@interface MainViewHandler : NSObject
+@interface MainViewHandler : NSObject <MainViewControllerHandler, KidozInitDelegate, KidozInterstitialDelegate, KidozRewardedDelegate, KidozBannerDelegate>
+
+-(void) setViewController:(BaseMainViewController*) baseMainViewController;
+-(NSString*) getAppLabel;
+-(NSString*) getSDKVersion;
+-(void) initSDK;
+-(void) onInitSuccess;
+-(void) onInitError:(NSString*) errorMessage;
+
+// MARK: - Banner
+-(void) loadBanner;
+-(void) hideBanner;
+-(void) onBannerAdLoaded;
+-(void) onBannerAdFailedToLoad:(KidozError*) error;
+-(void) onBannerAdShown;
+-(void) onBannerAdFailedToShow:(KidozError*) error;
+-(void) onBannerAdImpression;
+-(void) onBannerAdClosed;
+
+// MARK: - Interstitial ad
+-(void) loadInterstitial;
+-(void) showInterstitial;
+-(void) onInterstitialAdLoaded:(KidozInterstitialAd*) ad;
+-(void) onInterstitialAdFailedToLoad:(KidozError*) error;
+-(void) onInterstitialAdFailedToShow:(KidozError*) error;
+-(void) onInterstitialAdShown:(KidozInterstitialAd*) ad;
+-(void) onInterstitialImpression;
+-(void) onInterstitialAdClosed:(KidozInterstitialAd*) ad;
+
+// MARK: - Rewarded ad
+-(void) loadRewarded;
+-(void) showRewarded;
+-(void) onRewardedAdLoaded:(KidozRewardedAd*) ad;
+-(void) onRewardedAdFailedToLoad:(KidozError*) error;
+-(void) onRewardedAdFailedToShow:(KidozError*) error;
+-(void) onRewardedAdShown:(KidozRewardedAd*) ad;
+-(void) onRewardedImpression;
+-(void) onRewardReceived:(KidozRewardedAd*) ad;
+-(void) onRewardedAdClosed:(KidozRewardedAd*) ad;
+-(void) onTestCampaignIdsChange;
 
 @end
